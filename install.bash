6d31c2fa8ab168055c69e6149d78c58b34335c6a
CharlieBowen <charlesrbowen93@gmail.com>
2021-11-12 11:13:29 +0000

    chore: Added helpers

--- install.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm \
-    -u "$(id -u):$(id -g)" \
-    -v $(pwd):/var/www/html \
-    -w /var/www/html \
-    laravelsail/php80-composer:latest \
-    composer install --ignore-platform-reqs
