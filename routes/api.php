04e89a86cb59551e2f76740bab9378920714d00d
CharlieBowen <charlesrbowen93@gmail.com>
2021-11-12 10:44:09 +0000

    fix: Remove web routes

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/hello', function () {
+Route::get('/welcome', function () {
     return [
         'message' => 'Welcome to test driven development'
     ];
